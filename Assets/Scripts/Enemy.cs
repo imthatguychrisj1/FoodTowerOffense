e46cfa464d2d5471bcc29fa0da121d33d1edeffd
imthatguychrisj1 <imthatguychrisj@gmail.com>
2018-12-12 16:02:02 -0500

    Waypoints In progress

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public GameObject waypointContainer;
+    public float minimumDistance = 3f;
+
+    private Transform[] waypoints;
+    private int currentWaypoint;
+
+    void Start()
+    {
+        Transform[] potentialWaypoints = waypointContainer.GetComponentsInChildren<Transform>();
+        waypoints = new Transform[potentialWaypoints.Length - 1];
+
+        for (int i = 1; i < potentialWaypoints.Length; ++i)
+        {
+            waypoints[i - 1] = potentialWaypoints[i];
+        }
+    }
+
+    void Update()
+    {
+        if ((transform.position - waypoints[currentWaypoint].position).magnitude < minimumDistance)
+        {
+            currentWaypoint += 1;
+            if (currentWaypoint == waypoints.Length)
+            {
+                currentWaypoint = 0;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour {
+
+    public float size = 1f;
+    public Color color = Color.yellow;
+    private Transform[] waypoints;
+
+    private void OnDrawGizmos()
+    {
+        waypoints = GetComponentsInChildren<Transform>();
+        //Vector3 last = waypoints[waypoints.Length - 1].position;
+
+        for (int i = 1; i < waypoints.Length; i++)
+        {
+            Gizmos.color = color;
+            Gizmos.DrawSphere(waypoints[i].position, size);
+            //Gizmos.DrawLine(waypoints[i].position);
+            //last = waypoints[i].position;
+            waypoints[i].gameObject.name = "waypoint" + i;
+        }
+    }
+}
