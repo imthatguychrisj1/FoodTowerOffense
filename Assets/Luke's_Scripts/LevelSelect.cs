0a3e7e450161925c651da20213d5a5fc98eee6d0
Luke Malcolm <LMalcolm25@gmail.com>
2018-12-13 15:55:46 -0500

    TheMapSceneupdated
    Skybox Added
    Clickable Button Added
    LoadLevel Script Added
    MapScene almost Complete

---
+++ Assets/Luke's_Scripts/LevelSelect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelSelect : MonoBehaviour
+{
+    public string BuildScene;
+
+    void OnMouseDown()
+    {
+      SceneManager.LoadScene(BuildScene);
+      Debug.Log("Button Clicked");
+ 
+    }
+}
+
