077b05eefaf2a175647dcb63e2acd578c38dedf6
Luke Malcolm <LMalcolm25@gmail.com>
2018-12-18 18:44:44 -0500

    Luke & Chris Projects Merged
    Created Folders for all our stuff. Successfully moved Chris' stuff over to mine.

---
+++ Assets/ThatGuyChrisJ/Scripts_Chris/Waypoints.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waypoints : MonoBehaviour {
+
+    public GameObject[] Waypoints;
+    int current = 0;
+    public float speed;
+    float WPradius = 1;
+
+
+	// Update is called once per frame
+	void Update () {
+		
+        if (Vector3.Distance(Waypoints[current].transform.position, transform.position) < WPradius)
+        {
+            current++;
+
+            if (current >= Waypoints.Length)
+            {
+                current = 0;
+                Destroy(this.gameObject);
+            }
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, Waypoints[current].transform.position, Time.deltaTime * speed);
+	}
+}
