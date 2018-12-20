aaf777f04f7f24162adf6bfc1e5cb14dc8b67503
Luke Malcolm <LMalcolm25@gmail.com>
2018-12-20 16:52:42 -0500

    Updated
    Added Chris's Scripts

---
+++ Assets/Scripts/Chris/Health.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public Vector2 startPoint;
+    public Texture2D[] images;
+    public float hitPoints = 0f;
+    public float maxHitPoints = 0f;
+    public bool isPlayer = false;
+
+    public float HitPoints { get { return hitPoints; } set { hitPoints = Mathf.Clamp(value, 0, maxHitPoints); } }
+
+    public void ModifyHitPoints(int amount)
+    {
+        HitPoints += amount;
+    }
+
+    private Texture2D GetHealthBarImage(float normalizedHealth)
+    {
+        for (int i = 0; i < images.Length; ++i)
+        {
+            if (normalizedHealth >= 1f - (1f / (images.Length) * i))
+            {
+                return images[i];
+            }
+        }
+        return null;
+    }
+
+    private void OnGUI()
+    {
+        if (!isPlayer)
+        {
+            return;
+        }
+
+        float normalizedHealth = hitPoints / maxHitPoints;
+
+        GUILayout.BeginArea(new Rect(
+            startPoint.x,
+            startPoint.y,
+            Screen.width - startPoint.x,
+            Screen.height - startPoint.y));
+        GUILayout.Label(GetHealthBarImage(normalizedHealth));
+        GUILayout.EndArea();
+    }
+}
--- Assets/Scripts/Shared/waypoints.cs
+++ Assets/Scripts/Shared/waypoints.cs
@@ -2,30 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class waypoints : MonoBehaviour
-{
+public class Waypoints : MonoBehaviour {
 
-    public GameObject[] Waypoints;
+    public GameObject[] waypoints;
     int current = 0;
     public float speed;
-    float WPradius = 0.1f;
+    float WPradius = 1;
 
 
-    // Update is called once per frame
-    void Update()
-    {
-
-        if (Vector3.Distance(Waypoints[current].transform.position, transform.position) < WPradius)
+	// Update is called once per frame
+	void Update () {
+		
+        if (Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
         {
             current++;
 
-            if (current >= Waypoints.Length)
+            if (current >= waypoints.Length)
             {
                 current = 0;
                 Destroy(this.gameObject);
             }
         }
 
-        transform.position = Vector3.MoveTowards(transform.position, Waypoints[current].transform.position, Time.deltaTime * speed);
-    }
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+	}
 }
